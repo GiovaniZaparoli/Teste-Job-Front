db8d49fe65b980e296fa684dd91eecdcc95ba516
giovani.zaparoli <giovani.zaparoli@leavening.com>
2023-01-28 09:08:01 -0300

    Change env

--- src/service/env.js
+++ src/service/env.js
@@ -1,10 +1,10 @@
 export const getBaseURL = (project) => envsBaseUrl[project][env]
 
-const env = 'development'
+const env = 'production'
 
 const envsBaseUrl = {
   api: {
     development: 'https://giovani-job-test-api.herokuapp.com/api',
-    production: '',
+    production: 'https://giovani-job-test-api.herokuapp.com/api',
   },
 }
